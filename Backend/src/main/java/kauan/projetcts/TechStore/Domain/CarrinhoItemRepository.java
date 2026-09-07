88e13846fa9721ecf7151e2d8eb1d81290b2f92e
Kcode61 <kyocode61@gmail.com>
2026-09-07 20:00:35 -0300

    FIX BUGS, AGAIN

--- Backend/src/main/java/kauan/projetcts/TechStore/Domain/CarrinhoItemRepository.java
+++ Backend/src/main/java/kauan/projetcts/TechStore/Domain/CarrinhoItemRepository.java
@@ -2,6 +2,8 @@ package kauan.projetcts.TechStore.Domain;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface CarrinhoItemRepository extends JpaRepository<CarrinhoItem, Integer> {
+import java.util.List;
 
+public interface CarrinhoItemRepository extends JpaRepository<CarrinhoItem, Integer> {
+    List<CarrinhoItem> findByProduto_Id(int produtoId);
 }
